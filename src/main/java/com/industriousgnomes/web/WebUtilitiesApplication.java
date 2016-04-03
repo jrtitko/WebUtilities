d1c0f3b110619f963663e2855e6d5f5549c20307
J.R. Titko <j.r.titko.professional@gmail.com>
2016-04-03 08:25:50 -0400

    Initial Commit

---
+++ src/main/java/com/industriousgnomes/web/WebUtilitiesApplication.java
@@ -0,0 +1,12 @@
+package com.industriousgnomes.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebUtilitiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebUtilitiesApplication.class, args);
+	}
+}
